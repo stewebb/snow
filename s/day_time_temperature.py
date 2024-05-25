b2602129a33e9405e79e2a98b03e0696729a22a5
Steven Webb <u7544998@anu.edu.au>
2024-05-25 16:02:41 +1000

    c

--- s/day_time_temperature.py
+++ s/day_time_temperature.py
@@ -3,30 +3,49 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 import pandas as pd
 
-#from matplotlib.ticker import FuncFormatter
+'''
+    Simulate snow effects in a place during winter.
+    The place typically is located in temperate regions in the Northern Hemisphere.
+'''
 
-# Time formatter for the plot
-#def time_formatter(x, pos):
-#    hour = int(x // 60)
-#    minute = int(x % 60)
-#    return f'{hour:02d}:{minute:02d}'
-
-
+# Calculate snow amount based on temperature
+# Output range: [0, 1]. 0 -> No snow 1 -> Full snow
+def snow_amount(temperature):
+    if temperature <= 0:
+        return 1.0
+    elif 0 < temperature < 5:
+        return 1 - (temperature / 5.0)
+    else:
+        return 0.0
+    
 # Set x-axis to show 0-24 with integer labels
-xtick_values = np.linspace(0, 1440, 25)  # Positions where ticks will be placed
-xtick_labels = [int(label) for label in np.linspace(0, 24, 25)]  # Integer labels from 0 to 24
+xtick_values = np.linspace(0, 1440, 25)
+xtick_labels = [int(label) for label in np.linspace(0, 24, 25)]
 
-# Convert times to minutes in a day
-times_segments = np.array([0.0, 1.0, 2.0,  3.0,  4.0,  5.0,  6.0,  7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0]) * 60
-temps_segments = np.array([1.0, 0.0, 0.0, -1.0, -3.0, -4.0, -5.0, -2.0, 1.0, 3.0,  7.0, 10.0, 12.0, 13.0, 15.0, 16.0, 14.0, 12.0,  9.0,  7.0,  5.0,  4.0,  3.0,  2.0]) - 5.0
+# The time-temperature relationship
+times_segments = np.array([ 0,  1,  2,  3,  4,  5,   6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]) * 60
+temps_segments = np.array([-4, -5, -5, -6, -8, -9, -10, -7, -6, -2,  2,  5,  7,  9, 12, 10,  9,  7,  4,  2,  0, -1, -2, -3])
 
-# Use CubicSpline for a smoother interpolation
+# Time array for minutes in a day
+minute_times = np.linspace(0, 1440, 1440) 
+
+# Perform interpolation for temperatures
 cs = CubicSpline(times_segments, temps_segments, bc_type='natural')
-
-# Generate times in minutes
-minute_times = np.linspace(0, 1440, 1440)  # Time array for minutes in a day
 minute_temperatures = cs(minute_times)
 
+# Calculate snow amounts for the interpolated temperature values
+snow_amounts = np.vectorize(snow_amount)(minute_temperatures)
+
+data = pd.DataFrame({
+    'Minute': minute_times,
+    'Temperature': minute_temperatures,
+    'SnowAmount': snow_amounts
+})
+
+# Save to CSV for OpenGL C++ renderer
+data.to_csv('minute_data.csv', index=False)
+
+'''
 # Plotting
 plt.figure(figsize=(10, 5))
 plt.plot(minute_times, minute_temperatures, label='Temperature vs. Time', color='blue')
@@ -42,17 +61,7 @@ plt.grid(True)
 plt.legend()
 #plt.show()
 
-# Define the snow amount calculation function
-def calculate_snow_amount(temperature):
-    if temperature <= 0:
-        return 1.0
-    elif 0 < temperature < 5:
-        return 1 - (temperature / 5.0)
-    else:
-        return 0.0
 
-# Calculate snow amounts for the interpolated temperature values
-snow_amounts = np.vectorize(calculate_snow_amount)(minute_temperatures)
 
 # Plotting the results
 plt.figure(figsize=(12, 6))
@@ -66,14 +75,6 @@ plt.grid(True)
 plt.legend()
 
 # Create a DataFrame
-data = pd.DataFrame({
-    'Minute': minute_times,
-    'Temperature': minute_temperatures,
-    'SnowAmount': snow_amounts
-})
-
-# Save to CSV
-data.to_csv('minute_data.csv', index=False)
-
 
 plt.show()
+'''
