fd57ce8c0401198c246575cdcf5c19144b3968ab
Steven Webb <u7544998@anu.edu.au>
2024-05-30 01:06:17 +1000

    l

--- report/color_map_legend.py
+++ report/color_map_legend.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
 # Define the colors and their boundaries for both mappings
 colors = [
     (1.00, 0.50, 0.50),  # Red
@@ -16,17 +20,17 @@ vis_cmap = LinearSegmentedColormap.from_list("Visibility", colors)
 angle_cmap = LinearSegmentedColormap.from_list("AngleMap", colors)
 
 # Create a figure and subplots
-fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8, 3))
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(5, 3))
 
 # Color bar for visibility
-cb1 = fig.colorbar(plt.cm.ScalarMappable(cmap=vis_cmap), cax=axes[0], orientation='horizontal', label='Visibility')
+cb1 = fig.colorbar(plt.cm.ScalarMappable(cmap=vis_cmap), cax=axes[0], orientation='horizontal', label='Exposure value')
 cb1.set_ticks(np.linspace(0, 1, 11))
 cb1.set_ticklabels([f"{i:.1f}" for i in np.linspace(0, 1, 11)])
 
 # Color bar for angle color mapping
-cb2 = fig.colorbar(plt.cm.ScalarMappable(cmap=angle_cmap), cax=axes[1], orientation='horizontal', label='Dot Product (cosine of angle)')
+cb2 = fig.colorbar(plt.cm.ScalarMappable(cmap=angle_cmap), cax=axes[1], orientation='horizontal', label='Angle between N and U')
 cb2.set_ticks(angle_boundaries)
-cb2.set_ticklabels([">=90°", "75°-90°", "60°-75°", "45°-60°", "30°-45°", "15°-30°", "<15°"])
+cb2.set_ticklabels(["90°", "75°", "60°", "45°", "30°", "15°", "0°"])
 
 plt.tight_layout()
 plt.show()
